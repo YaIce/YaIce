be15ba2a68eb615fcc2658e7b52db36e82643a30
Yang Min <yangmin@gyyx.com>
2024-12-30 21:48:01 +0800

    fix：update

--- network/IConn.go
+++ network/IConn.go
@@ -1,6 +1,7 @@
 package network
 
 import (
+	"github.com/yaice-rx/yaice/router"
 	"google.golang.org/protobuf/proto"
 )
 
@@ -11,6 +12,7 @@ type IConn interface {
 	Send(message proto.Message) error
 	SendByte(message []byte) error
 	GetConn() interface{}
+	getRouter() router.IRouter
 	GetIsPos() int64
 	GetCreateTime() int64
 	GetOptions() IOptions
